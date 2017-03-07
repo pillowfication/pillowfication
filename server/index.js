d110f2a548f289431ba5a6919643a6c13be6936e
Markus Tran <thebluepillow@gmail.com>
2017-03-07 12:55:58 -0800

    add compress

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const express = require('express');
+const compression = require('compression');
 
 const app = express();
 const port = +process.argv[2] || 80;
+
+app.use(compression());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../website/index.html'));
@@ -10,9 +13,13 @@ app.get('/', (req, res) => {
 app.get('/bundle.js', (req, res) => {
   res.sendFile(path.join(__dirname, '../website/bundle.js'));
 });
-app.get('/bundle.js.map', (req, res) => {
-  res.sendFile(path.join(__dirname, '../website/bundle.js.map'));
+app.get('/bundle.js.gz', (req, res) => {
+  res.set('Content-Encoding', 'gzip');
+  res.sendFile(path.join(__dirname, '../website/bundle.js.gz'));
 });
+// app.get('/bundle.js.map', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../website/bundle.js.map'));
+// });
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, '/website/index.jsx'),
@@ -12,5 +13,14 @@ module.exports = {
       {test: /\.jsx?$/, loader: 'babel-loader'},
       {test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader'},
     ]
-  }
+  },
+  plugins: [
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
 };
