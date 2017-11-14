2cf00a0679e13755a2c0dbe2821804498566c0b1
Markus Tran <thebluepillow@gmail.com>
2017-11-13 21:22:51 -0800

    add router to github

--- src/github/App.jsx
+++ src/github/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Home from './components/Home.jsx'
+import _404 from './components/404.jsx'
+import PFSowpods from './components/PFSowpods.jsx'
 
 export default class App extends React.Component {
   render () {
-    return <div>GitHub</div>
+    return (
+      <BrowserRouter basename='/github'>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/pf-sowpods' component={PFSowpods} />
+          <Route component={_404} />
+        </Switch>
+      </BrowserRouter>
+    )
   }
 }
---
+++ src/github/components/PFSowpods.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class PFSowpods extends React.Component {
+  render () {
+    return <div>Sowpods</div>
+  }
+}
