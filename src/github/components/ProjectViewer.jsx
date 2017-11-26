653452597c87e9c8feaaaf4f67f8b88e14f783e1
Markus Tran <thebluepillow@gmail.com>
2017-11-26 11:17:04 -0800

    organize routes

--- src/github/components/ProjectViewer.jsx
+++ src/github/components/ProjectViewer.jsx
@@ -11,30 +11,45 @@ import PFKonami from './pf-konami/PFKonami.jsx'
 import zf from '../foundation.scss'
 import styles from './ProjectViewer.scss'
 
+// TODO: add `/{org}` for other ROUTE groups
+const ROUTES = [{
+  header: 'pillowfication',
+  children: [
+    { path: 'awesome-dings', component: AwesomeDings },
+    { path: 'cis89c', component: CIS89C },
+    { path: 'perfect-cuboid', component: PerfectCuboid },
+    { path: 'pf-konami', component: PFKonami }
+  ]
+}]
+
 class ProjectViewer extends Component {
   render () {
     return (
       <div className={classnames(styles.projectViewer, zf.row, zf.columns, zf.small12)}>
         <nav className={styles.sideBar}>
           <ul className={classnames(zf.menu, zf.vertical)}>
-            <li>
-              pillowfication<span className={styles.pathDivider} />
-              <ul className={classnames(zf.menu, zf.vertical, zf.nested)}>
-                <li><NavLink activeClassName={styles.active} to='/awesome-dings'>awesome-dings</NavLink></li>
-                <li><NavLink activeClassName={styles.active} to='/cis89c'>cis89c</NavLink></li>
-                <li><NavLink activeClassName={styles.active} to='/perfect-cuboid'>perfect-cuboid</NavLink></li>
-                <li><NavLink activeClassName={styles.active} to='/pf-konami'>pf-konami</NavLink></li>
-              </ul>
-            </li>
+            {ROUTES.map(routeGroup =>
+              <li key={routeGroup.header}>
+                {routeGroup.header}<span className={styles.pathDivider} />
+                <ul className={classnames(zf.menu, zf.vertical, zf.nested)}>
+                  {routeGroup.children.map(route =>
+                    <li key={route.path}>
+                      <NavLink activeClassName={styles.active} to={'/' + route.path}>{route.path}</NavLink>
+                    </li>
+                  )}
+                </ul>
+              </li>
+            )}
           </ul>
         </nav>
         <main className={styles.main}>
           <Switch>
             <Route exact path='/' component={Home} />
-            <Route path='/awesome-dings' component={AwesomeDings} />
-            <Route path='/cis89c' component={CIS89C} />
-            <Route path='/perfect-cuboid' component={PerfectCuboid} />
-            <Route path='/pf-konami' component={PFKonami} />
+            {ROUTES.reduce((routes, routeGroup) =>
+              routes.concat(routeGroup.children.map(route =>
+                <Route key={route.path} path={'/' + route.path} component={route.component} />
+              ))
+            , [])}
             <Route component={_404} />
           </Switch>
         </main>
