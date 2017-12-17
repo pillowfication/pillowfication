c62612b04dbf8f99c08f72064b14023aa7e9e0a6
Markus Tran <thebluepillow@gmail.com>
2017-12-17 07:10:15 -0800

    bring back Nav

--- src/github/components/Navigation.jsx
+++ src/github/components/Navigation.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+import routes from './routes'
+
+import styles from './Navigation.scss'
 
 class Navigation extends Component {
   render () {
     return (
-      <nav>
-        asdf
+      <nav className={styles.nav}>
+        <ul>
+          {routes.map(routeGroup =>
+            <li key={routeGroup.header}>
+              <b>{routeGroup.header}</b><span className={styles.pathDivider} />
+              <ul>
+                {routeGroup.children.map(route =>
+                  <li key={route.path}>
+                    <NavLink activeClassName={styles.active} to={route.fullPath}>{route.path}</NavLink>
+                  </li>
+                )}
+              </ul>
+            </li>
+          )}
+        </ul>
       </nav>
     )
   }
